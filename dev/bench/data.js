cc82cde97904a1d99e3a3c2b26840cbd06d67cfd
github-action-benchmark <github@users.noreply.github.com>
2025-12-02 15:27:21 +0000

    add Benchmark (cargo) benchmark result for b9d638a0a06fdbb1367d01f63e8887c11586c47a

---
+++ dev/bench/data.js
@@ -0,0 +1,98 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1764689241877,
+  "repoUrl": "https://github.com/uselessgoddess/dunes",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "68294279+uselessgoddess@users.noreply.github.com",
+            "name": "uselessgoddess",
+            "username": "uselessgoddess"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b9d638a0a06fdbb1367d01f63e8887c11586c47a",
+          "message": "Merge pull request #13 from konard/issue-9-2fa720aa7697\n\nFix git identity configuration for gh-pages branch creation",
+          "timestamp": "2025-12-02T18:24:33+03:00",
+          "tree_id": "f24763708b848d59d63f0fcbe31f03b7cf504b6f",
+          "url": "https://github.com/uselessgoddess/dunes/commit/b9d638a0a06fdbb1367d01f63e8887c11586c47a"
+        },
+        "date": 1764689241470,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "sbt_insert_100/usize/100",
+            "value": 1833,
+            "range": "± 7",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sbt_insert_100/nonzero/100",
+            "value": 1441,
+            "range": "± 17",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sbt_insert_1000/usize/1000",
+            "value": 26110,
+            "range": "± 2258",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sbt_insert_1000/nonzero/1000",
+            "value": 20146,
+            "range": "± 792",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sbt_insert_10000/usize/10000",
+            "value": 325232,
+            "range": "± 1592",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sbt_insert_10000/nonzero/10000",
+            "value": 266493,
+            "range": "± 1435",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sbt_insert_search_100/usize/100",
+            "value": 2377,
+            "range": "± 18",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sbt_insert_search_1000/usize/1000",
+            "value": 36594,
+            "range": "± 283",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sbt_insert_search_10000/usize/10000",
+            "value": 745583,
+            "range": "± 2811",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sbt_full_cycle_100/usize/100",
+            "value": 1905,
+            "range": "± 5",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sbt_full_cycle_1000/usize/1000",
+            "value": 25673,
+            "range": "± 248",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
